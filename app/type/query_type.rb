1bc381f8dd8a1ebf564f470c5a77b377102c8fd6
Vinícius Pereira <jedipereira@gmail.com>
2017-06-17 12:31:01 -0300

    add new query option -- movies by year

--- app/type/query_type.rb
+++ app/type/query_type.rb
@@ -2,6 +2,7 @@ QueryType = GraphQL::ObjectType.define do
     name "Query"
     description "The query root for this GraphQL schema"
 
+    # Find Movie by ID
     field :movie do
         type MovieType
         argument :id, !types.ID
@@ -11,12 +12,27 @@ QueryType = GraphQL::ObjectType.define do
         }
     end
 
+    # Find Actor by ID
     field :actor do
         type ActorType
         argument :id, !types.ID
 
         resolve -> (obj, args, ctx) {
             Actor.find(args[:id])
+        }
+    end
+
+    # Find Movies by year
+    field :movies do
+        type types[MovieType]
+        argument :year, types.Int
+        
+        resolve -> (obj, args, ctx) {
+            if args[:year].present?
+                Movie.where(year: args[:year])
+            else
+                Movie.all
+            end
         }
     end
 end
