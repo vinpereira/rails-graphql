020b46dd4e174414b6f75ef2758a1a51afe0c698
Vinícius Pereira <jedipereira@gmail.com>
2017-06-17 12:13:20 -0300

    create Join Table migration (Many to Many)

---
+++ db/migrate/20170617150750_create_join_table_actor_movie.rb
@@ -0,0 +1,8 @@
+class CreateJoinTableActorMovie < ActiveRecord::Migration[5.1]
+  def change
+    create_join_table :actors, :movies do |t|
+      t.index [:actor_id, :movie_id]
+      t.index [:movie_id, :actor_id]
+    end
+  end
+end
