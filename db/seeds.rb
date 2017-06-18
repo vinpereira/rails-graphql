1a501a05315d7c1e3bfa78bae980b0535f850848
Vinícius Pereira <jedipereira@gmail.com>
2017-06-18 09:40:09 -0300

    put some data at db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+actor_ford = Actor.create!(name: "Harrison Ford", bio: "Some long biography about this actor")
+actress_fisher = Actor.create!(name: "Carrie Fisher", bio: "Some long biography about this actress")
+actor_hamill = Actor.create!(name: "Mark Hamill", bio: "Some long biography about this actor")
+
+movie = Movie.create!(title: "Star Wars Episode IV", year: 1977, summary: "A New Hope")
+movie.actors << actor_ford
+movie.actors << actress_fisher
+movie.actors << actor_hamill
+
+movie = Movie.create!(title: "Star Wars Episode V", year: 1980, summary: "The Empire Strikes Back")
+movie.actors << actor_ford
+movie.actors << actress_fisher
+movie.actors << actor_hamill
+
+movie = Movie.create!(title: "Indiana Jones", year: 1981, summary: "Raiders of the Lost Ark")
+movie.actors << actor_ford
+
+movie = Movie.create!(title: "Star Wars Episode VI", year: 1983, summary: "Return of Jedi")
+movie.actors << actor_ford
+movie.actors << actress_fisher
+movie.actors << actor_hamill
